985c632b2946333b24a3cceadb5e2e17fc5608ce
sanjoy5656 <sanjoyacharjee13@gmail.com>
2022-01-12 21:44:00 +0600

    day1

---
+++ test.py
@@ -0,0 +1,23 @@
+print("I love python")
+print('I am Sanjoy Acharjee')
+print('='*30)
+print('+'*50)
+print('I love '+'Bangladesh')
+print('10'+'5')
+print(20+30+5+2)
+
+
+x=10
+print(x)
+y=3.66
+print(y)
+
+print(type(x))
+print(type(y))
+s='ebiddaloy'
+print(s)
+print(type(s))
+
+isActive=True
+print(isActive)
+print(type(isActive))
